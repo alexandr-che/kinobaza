6a0b8eb470ecef38fff8492e2162a6ba023bbd21
Alexandr Cherdantsev <alex.che.prg@gmail.com>
2024-04-23 21:14:26 +0400

    добавил фильтр для MovieList

---
+++ kinobaza/movies/filters.py
@@ -0,0 +1,32 @@
+import django_filters
+
+from movies.models import Movie, Genre, Country
+
+
+class MovieFilter(django_filters.FilterSet):
+    year = django_filters.MultipleChoiceFilter(
+        choices=[
+            (year, year) for year in Movie.objects.values_list(
+                'year', flat=True).distinct().order_by('year')
+        ],
+        method='filter_by_year'
+    )
+
+    genre = django_filters.ModelMultipleChoiceFilter(
+        field_name = 'genre__name',
+        lookup_expr = 'icontains',
+        queryset = Genre.objects.all()
+    )
+
+    country = django_filters.ModelMultipleChoiceFilter(
+        field_name = 'country__name',
+        lookup_expr = 'icontains',
+        queryset = Country.objects.all()
+    )
+
+    class Meta:
+        model = Movie
+        fields = ['year', 'genre', 'country']
+
+    def filter_by_year(self, queryset, name, value):
+        return queryset.filter(**{f'{name}__in': value})
