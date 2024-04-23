5acc6729351efce46993e5eab7d84a0269b6dad0
Alexandr Cherdantsev <alex.che.prg@gmail.com>
2024-04-23 21:14:26 +0400

    добавил фильтр для MovieList

--- kinobaza/movies/urls.py
+++ kinobaza/movies/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
+from django_filters.views import FilterView
+
 from movies.views import MovieDetailView, MovieListView
+from movies.models import Movie
 
 
 app_name = 'movies'
 
 
 urlpatterns = [
-    path('', MovieListView.as_view(), name='movie_list'),
+    path('', MovieListView.as_view(model=Movie), name='movie_list'),
     path('find', MovieListView.as_view(), name='movie_list_find'),
+    path('filter/', FilterView.as_view(model=Movie), name='movie_filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
 ]
